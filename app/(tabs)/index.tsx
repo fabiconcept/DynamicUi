d7e006fe43baa05c346dd6dc5779400b92811934
fabiconcept <favourajokubi@gmail.com>
2024-12-28 12:15:26 +0100

    - Part 39 : Dynamic User Interfaces ✅

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,33 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-
+      <View style={styles.box}>
+        <Text style={styles.text}>Welcome Hi!</Text>
+      </View>
     </View>
   );
 }
+
+const windowsWidth = Dimensions.get("window").width;
+const windowsHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'plum',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: windowsWidth < 300 ? 100 : 300,
+    height: windowsHeight < 300 ? 100 : 300,
+    backgroundColor: 'lightblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: windowsWidth < 300 ? 10 :24,
   }
 });
