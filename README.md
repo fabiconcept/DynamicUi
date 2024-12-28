48e07ab6bf994d98ac16032831b640b7cc557eb5
fabiconcept <favourajokubi@gmail.com>
2024-12-28 12:39:23 +0100

    - Part 40 : Dimensions API ✅ - Part 41 : Dimensions API Drawbacks ✅

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [https://www.youtube.com/watch?v=ny7ZWVt3ymo&list=PLC3y8-rFHvwhiQJD1di4eRVN30WWCXkg1&index=3](Visit Link)
 
 - Part 39 : Dynamic User Interfaces ✅
+- Part 40 : Dimensions API ✅
+- Part 41 : Dimensions API Drawbacks ✅
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,34 @@
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 export default function HomeScreen() {
+  const [dimensions, setDimensions] = useState(Dimensions.get('window'));
+
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener('change', (newDimensions) => {
+      setDimensions(newDimensions.window);
+    })
+
+    return () => subscription?.remove();
+  }, []);
+
+  const { width, height } = dimensions;
+  const isLargeScreen = width > 500;
+  const isSmallScreen = width < 300;
+  const isMediumScreen = width >= 300 && width < 500;
+
+  const isTallScreen = height > 600;
+
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome Hi!</Text>
+      <View style={[styles.box, isLargeScreen ? styles.box_w_lg : styles.box_w_md, isTallScreen ? styles.box_h_md : styles.box_h_lg]}>
+        <Text style={[isLargeScreen && styles.text_xl, isMediumScreen && styles.text_lg, isSmallScreen && styles.text_sm]}>Welcome Hi!</Text>
+        <Text style={[isLargeScreen && styles.text_xl, isMediumScreen && styles.text_lg, isSmallScreen && styles.text_sm]}>{width}, {height}</Text>
       </View>
     </View>
   );
 }
-
-const windowsWidth = Dimensions.get("window").width;
-const windowsHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
   container: {
@@ -21,13 +38,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   box: {
-    width: windowsWidth < 300 ? 100 : 300,
-    height: windowsHeight < 300 ? 100 : 300,
     backgroundColor: 'lightblue',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text: {
-    fontSize: windowsWidth < 300 ? 10 :24,
-  }
+  box_w_lg: {
+    width: "90%",
+  },
+  box_h_lg: {
+    height: "90%",
+  },
+  box_w_md: {
+    width: "70%",
+  },
+  box_h_md: {
+    height: "60%",
+  },
+  text_lg: {
+    fontSize: 24,
+  },
+  text_sm: {
+    fontSize: 12,
+  },
+  text_xl: {
+    fontSize: 36,
+  },
 });
